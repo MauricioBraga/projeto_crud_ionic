5ba168ea5530667aae1975cdaf3a9d01e2ea3437
Mauricio Braga <braga.mauricio@gmail.com>
2024-09-11 08:46:09 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'projeto_crud_ionic',
+  webDir: 'www'
+};
+
+export default config;
