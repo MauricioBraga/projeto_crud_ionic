8cb2df1f3b2e74fbb67d23b01c38834d9825e019
Mauricio Braga <braga.mauricio@gmail.com>
2024-09-18 13:55:53 -0300

    adicionado o consultarConta

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,86 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  rep: Conta[] = [];
+  c2: Conta;
+  numero: string = '';
+  nome: string = '';
+  saldo: number = 0;
 
-  constructor() {}
+  constructor() {
 
+    this.limpa_campos();
+    // conta de teste.
+    this.c2 = new Conta('10', 'Beatriz Maria Ferreira', 30000);
+    this.rep.push(this.c2);
+  }
+
+  public consultarConta(): void {
+
+
+    let encontrado: boolean = false;
+    for (let i: number = 0; i < this.rep.length; i++) {
+      let c: Conta = this.rep[i];
+      if (c.getNumero() == this.numero) {
+        this.nome = c.getNome();
+        this.saldo = c.getSaldo();
+        encontrado = true;
+        return;
+      }
+    }
+    if (!encontrado) {
+      console.log('Conta não localizada');
+      alert('Conta não localizada');
+      this.limpa_campos();
+    }
+  }
+
+  public inserirConta(): void {
+
+  }
+  public alterarConta(): void {}
+
+  public removerConta(): void {}
+
+  public limpa_campos(): void {
+    this.numero = '';
+    this.nome = '';
+    this.saldo = 0;
+  }
+}
+
+export default class Conta {
+  numero: string;
+  nome: string;
+  saldo: number;
+
+  constructor(numero: string, nome: string, saldo: number) {
+    this.numero = numero;
+    this.nome = nome;
+    this.saldo = saldo;
+  }
+
+  public getNome(): string {
+    return this.nome;
+  }
+
+  public getNumero(): string {
+    return this.numero;
+  }
+
+  public getSaldo(): number {
+    return this.saldo;
+  }
+
+  public setNome(nome: string): void {
+    this.nome = nome;
+  }
+
+  public setNumero(numero: string): void {
+    this.numero = numero;
+  }
+
+  public setSaldo(saldo: number): void {
+    this.saldo = saldo;
+  }
 }
